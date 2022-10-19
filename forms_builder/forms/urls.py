c379d1e35b9f96ffb92fd12bec74b99ab3b9f95e
Robert Barsch <barsch@egu.eu>
2022-10-19 09:41:14 +0200

    fix urls

--- forms_builder/forms/urls.py
+++ forms_builder/forms/urls.py
@@ -1,9 +1,9 @@
-from django.urls import re_path
+from django.urls import path
 
 from forms_builder.forms import views
 
 
 urlpatterns = [
-    re_path(r"(?P<slug>.*)/sent/$", views.form_sent, name="form_sent"),
-    re_path(r"(?P<slug>.*)/$", views.form_detail, name="form_detail"),
+    path("<slug>/sent/", views.form_sent, name="form_sent"),
+    path("<slug>/", views.form_detail, name="form_detail"),
 ]
